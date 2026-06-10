61aaaceea3697bc83c83f6d2fbebd945e4d80966
PasinS47 <pasinaugus47@gmail.com>
2026-06-10 11:47:00 +0700

    fix: register error handling and UI size adjustment

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource react */
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Form, Input, Button, Card, Typography, Alert } from 'antd'
+import { Form, Input, Button, Card, Typography, Alert, ConfigProvider } from 'antd'
 import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons'
 import api from '../api/axios'
 
@@ -20,13 +20,15 @@ export default function RegisterPage() {
       localStorage.setItem('jwt_token', response.data.token)
       navigate('/dashboard')
     } catch (error: any) {
+      const backendMessage = error.response.data[0]
       if (error.response?.status === 400) {
-        const backendMessage = error.response.data
         setServerError(
           typeof backendMessage === 'string' ? backendMessage : 'Invalid register information.',
         )
       } else {
-        setServerError('An unexpected error occurred. Please try again later.')
+        setServerError(
+          typeof backendMessage === 'string' ? backendMessage : 'Email or username already exists.',
+        )
       }
     } finally {
       setLoading(false)
@@ -56,93 +58,94 @@ export default function RegisterPage() {
         {serverError && (
           <Alert message={serverError} type="error" showIcon style={{ marginBottom: 24 }} />
         )}
-
-        <Form
-          name="register"
-          onFinish={onFinish}
-          layout="vertical"
-          requiredMark={true}
-          size="large"
-        >
-          <Form.Item
-            name="firstName"
-            label="First name"
-            rules={[{ required: true, message: 'First name is required' }]}
+        <ConfigProvider theme={{ components: { Form: { itemMarginBottom: 0 } } }}>
+          <Form
+            name="register"
+            onFinish={onFinish}
+            layout="vertical"
+            requiredMark={true}
+            size="large"
           >
-            <Input placeholder="Enter your first name" />
-          </Form.Item>
-          <Form.Item
-            name="lastName"
-            label="Last name"
-            rules={[{ required: true, message: 'Last name is required' }]}
-          >
-            <Input placeholder="Enter your last name" />
-          </Form.Item>
-          <Form.Item
-            name="username"
-            label="Username"
-            rules={[{ required: true, message: 'Username is required' }]}
-          >
-            <Input prefix={<UserOutlined />} placeholder="Enter your username" />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            label="Email"
-            rules={[
-              { required: true, message: 'Email is required' },
-              { type: 'email', message: 'Invalid email format' },
-            ]}
-          >
-            <Input prefix={<MailOutlined />} placeholder="Enter your email" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-              { required: true, message: 'Password is required' },
-              { min: 8, message: 'Password must be at least 8 characters' },
-              {
-                pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                message:
-                  'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password prefix={<LockOutlined />} placeholder="••••••••" />
-          </Form.Item>
-          <Form.Item
-            name="confirmPassword"
-            label="Confirm password"
-            rules={[
-              { required: true, message: 'Confirm your password!' },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) return Promise.resolve()
-                  return Promise.reject(new Error('The two passwords do not match!'))
+            <Form.Item
+              name="firstName"
+              label="First name"
+              rules={[{ required: true, message: 'First name is required' }]}
+            >
+              <Input placeholder="Enter your first name" />
+            </Form.Item>
+            <Form.Item
+              name="lastName"
+              label="Last name"
+              rules={[{ required: true, message: 'Last name is required' }]}
+            >
+              <Input placeholder="Enter your last name" />
+            </Form.Item>
+            <Form.Item
+              name="username"
+              label="Username"
+              rules={[{ required: true, message: 'Username is required' }]}
+            >
+              <Input prefix={<UserOutlined />} placeholder="Enter your username" />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              label="Email"
+              rules={[
+                { required: true, message: 'Email is required' },
+                { type: 'email', message: 'Invalid email format' },
+              ]}
+            >
+              <Input prefix={<MailOutlined />} placeholder="Enter your email" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[
+                { required: true, message: 'Password is required' },
+                { min: 8, message: 'Password must be at least 8 characters' },
+                {
+                  pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                  message:
+                    'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
                 },
-              }),
-            ]}
-            hasFeedback
-          >
-            <Input.Password prefix={<LockOutlined />} placeholder="••••••••" />
-          </Form.Item>
-          <Form.Item style={{ marginTop: 32, marginBottom: 0 }}>
-            <Button type="primary" htmlType="submit" block loading={loading}>
-              Register
-            </Button>
-          </Form.Item>
-          <Form.Item
-            style={{
-              marginBottom: 0,
-              display: 'flex',
-              justifyContent: 'center',
-              alignContent: 'bottom',
-            }}
-          >
-            Already have an account? <a href="/login">Login</a>
-          </Form.Item>
-        </Form>
+              ]}
+              hasFeedback
+            >
+              <Input.Password prefix={<LockOutlined />} placeholder="••••••••" />
+            </Form.Item>
+            <Form.Item
+              name="confirmPassword"
+              label="Confirm password"
+              rules={[
+                { required: true, message: 'Confirm your password!' },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) return Promise.resolve()
+                    return Promise.reject(new Error('The two passwords do not match!'))
+                  },
+                }),
+              ]}
+              hasFeedback
+            >
+              <Input.Password prefix={<LockOutlined />} placeholder="••••••••" />
+            </Form.Item>
+            <Form.Item style={{ marginTop: 32, marginBottom: 0 }}>
+              <Button type="primary" htmlType="submit" block loading={loading}>
+                Register
+              </Button>
+            </Form.Item>
+            <Form.Item
+              style={{
+                marginBottom: 0,
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'bottom',
+              }}
+            >
+              Already have an account? <a href="/login">Register</a>
+            </Form.Item>
+          </Form>
+        </ConfigProvider>
       </Card>
     </div>
   )
